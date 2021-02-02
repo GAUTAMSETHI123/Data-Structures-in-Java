8a4fd4d0c54195a43e6a2b9859e397aaefeb2459
gautam <gautam.sethi_cs19@gla.ac.in>
2021-02-03 01:07:37 +0530

    array operation

---
+++ src/Scratch/ArrayOps.java
@@ -0,0 +1,48 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 03-02-2021
+ *   Time: 00:40
+ *   File: ArrayOps.java
+ */
+
+package Scratch;
+
+import java.util.Scanner;
+
+public class ArrayOps {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("enter the size of the array");
+        int n = scan.nextInt();
+
+        int [] array = new int[n];
+
+
+       int[] arr1 = takeInput(array);
+       display(arr1);
+
+
+
+    }
+
+    public static int[] takeInput(int [] arr){
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("the value at index " + i + "is");
+            arr[i] = scan.nextInt();
+
+        }
+        return arr;
+    }
+
+    public static void display(int[] arr){
+        for (int val : arr) {
+            System.out.println(val);
+
+        }
+
+    }
+}
+
+
