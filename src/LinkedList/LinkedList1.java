19746a33bbff434d77df0c520dcdc5e1b0e46f50
gautam <gautam.sethi_cs19@gla.ac.in>
2021-02-12 02:35:04 +0530

    made a class Node and a function display() in LinkedList1.java for traversal in a linked list.

---
+++ src/LinkedList/LinkedList1.java
@@ -0,0 +1,32 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 12-02-2021
+ *   Time: 02:27
+ *   File: LinkedList1.java
+ */
+
+package LinkedList;
+
+public class LinkedList1 {
+
+    private class Node{
+         int data;
+         Node next;
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+
+    public void display(){
+        Node temp = this.head;
+        while (temp!=null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+
+
+}
