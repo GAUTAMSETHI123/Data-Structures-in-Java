a2a809c6ff43a46328a0d5a8ea292e9af2380432
gautam <gautam.sethi_cs19@gla.ac.in>
2021-02-27 22:02:34 +0530

    made a main class in LinkedList1.java

---
+++ src/Scratch/BinarySearch.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 05-02-2021
+ *   Time: 02:56
+ *   File: BinarySearch.java
+ */
+
+package Scratch;
+
+public class BinarySearch {
+}
