b0ca1c00281e9bdf13ae36da630cea6f116441ac
gautam <gautam.sethi_cs19@gla.ac.in>
2021-04-30 12:16:26 +0530

    implemented Binary Search in a narray

--- src/Scratch/BinarySearch.java
+++ src/Scratch/BinarySearch.java
@@ -7,5 +7,29 @@
 
 package Scratch;
 
+import static java.util.Arrays.binarySearch;
+
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 14, 17, 23, 34, 56, 58, 60, 61};
+        System.out.println(binarySearch(arr, 67));
+    }
+
+
+    public static int binarySearch(int[]arr1, int item){
+        int li = 0;
+        int hi = arr1.length -1;
+
+        while(li<hi){
+            int mid = (li+hi)/2;
+            if(arr1[mid]== item)
+                return mid;
+            else if(arr1[mid] < item)
+                 li=mid+1;
+            else
+                 hi = mid-1;
+
+        }
+        return -1;
+    }
 }
