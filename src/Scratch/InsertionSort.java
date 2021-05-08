c9e79b503e70857fb621e8dc81d867e8eed7fea1
gautam <gautam.sethi_cs19@gla.ac.in>
2021-05-09 02:30:05 +0530

    Implemented insertion sort in an array.

---
+++ src/Scratch/InsertionSort.java
@@ -0,0 +1,60 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 09-05-2021
+ *   Time: 01:55
+ *   File: InsertionSort.java
+ */
+
+package Scratch;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the size of the array");
+        int N = scan.nextInt();
+        int[] arr = new int[N];
+
+        for (int k = 0; k < arr.length; k++) {
+            arr[k] = scan.nextInt();
+        }
+
+        System.out.println("Array is");
+        for (int val: arr
+        ) {
+            System.out.print(val + " ");
+        }
+
+        System.out.println();
+        int[] ret = insertionSort(arr);
+        System.out.println(" The sorted array is");
+        for (int val1 : ret
+        ) {
+            System.out.print(val1 + " ");
+
+        }
+
+    }
+
+    public static int[] insertionSort(int[] arr1){
+        for(int i =1; i<arr1.length; i++){
+            int key = arr1[i];
+            int j = i-1;
+            while(j>=0 && arr1[j]> key ){
+                arr1[j+1] = arr1[j];
+
+
+                j--;
+            }
+            arr1[j+1] = key;
+
+        }
+        return arr1;
+    }
+
+
+}
+
+
+
