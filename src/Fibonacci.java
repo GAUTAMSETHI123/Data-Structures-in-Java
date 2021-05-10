bf4f12970baaa0ae4829310587ad444bc76bbf48
gautam <gautam.sethi_cs19@gla.ac.in>
2021-05-10 23:37:59 +0530

    print the fibonacci series

---
+++ src/Fibonacci.java
@@ -0,0 +1,27 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 10-05-2021
+ *   Time: 23:15
+ *   File: Fibonacci.java
+ */
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the number");
+        int N = scan.nextInt();
+        int a =0;
+        int b = 1;
+
+        int counter = 1;
+        while(counter <= N+1){
+            System.out.println(a);
+            int sum = a+b;
+            a=b;
+            b=sum;
+            counter++;
+        }
+
+    }
+
+}
