4458d93c5c4ae61d875a2292eb0547ea27f8ccfd
gautam <gautam.sethi_cs19@gla.ac.in>
2021-05-11 19:46:31 +0530

    program to print a straight pattern in Pattern1.java

---
+++ src/Pattern/Pattern1.java
@@ -0,0 +1,24 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 11-05-2021
+ *   Time: 19:42
+ *   File: Pattern1.java
+ */
+
+package Pattern;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter the number of rows");
+        int n = scn.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+
+        }
+
+
+    }
+}
