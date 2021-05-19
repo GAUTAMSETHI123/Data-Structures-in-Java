7d35413d382501f554465e9aa4f2547ba6b31e83
gautam <gautam.sethi_cs19@gla.ac.in>
2021-05-19 19:04:12 +0530

    simple function

---
+++ src/Function/FunctionDemo.java
@@ -0,0 +1,25 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 19-05-2021
+ *   Time: 18:49
+ *   File: FunctionDemo.java
+ */
+
+package Function;
+import java.util.Scanner;
+
+public class FunctionDemo {
+    public static void main(String[] args) {
+        addition(15,87);
+
+    }
+
+
+    public static void addition(int a , int b){
+
+        System.out.println(a+b);
+
+        }
+
+    }
+
