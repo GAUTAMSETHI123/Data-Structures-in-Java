fcf51205e9c00393025e0ff532943e06cb7efe21
gautam <gautam.sethi_cs19@gla.ac.in>
2021-05-20 01:42:59 +0530

    use of function in a program called AnybaseToBinary.java

---
+++ src/Function/AnybaseToBinary.java
@@ -0,0 +1,35 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 20-05-2021
+ *   Time: 01:29
+ *   File: AnybaseToBinary.java
+ */
+
+package Function;
+
+import java.util.Scanner;
+
+public class AnybaseToBinary {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int src = scn.nextInt();
+        int srcB = scn.nextInt();
+        int res = anybaseToDecimal(src,srcB);
+        System.out.println(res);
+
+    }
+
+
+    public static int anybaseToDecimal(int srcNo , int srcBase){
+        int multiplier = 1;
+        int ans =0;
+        while(srcNo!=0){
+            int rem = srcNo%10;
+            ans = ans + (rem*multiplier);
+            multiplier*=srcBase;
+            srcNo/=10;
+
+        }
+        return ans;
+    }
+}
