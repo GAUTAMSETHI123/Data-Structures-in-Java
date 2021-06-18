866e64dfdb8e43a431665c82f1905d6e697f20f0
gautam <gautam.sethi_cs19@gla.ac.in>
2021-06-19 02:22:50 +0530

    prime number

---
+++ src/Scratch/CheckPrime.java
@@ -0,0 +1,41 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 19-06-2021
+ *   Time: 01:59
+ *   File: CheckPrime.java
+ */
+
+package Scratch;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean flag = true;
+        for(int i=2; i<=n/2; i++ ){
+            if(n%i == 0){
+                flag = false;
+                break;
+            }
+
+            }
+        if(flag){
+            System.out.println("number is prime");
+        }
+        else{
+            System.out.println("number is not prime");
+        }
+
+
+
+            }
+
+
+        }
+
+
+
+
+
