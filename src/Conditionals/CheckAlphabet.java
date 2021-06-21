fbb58c682ef011053be2532f5cf0f00d8ed066a7
gautam <gautam.sethi_cs19@gla.ac.in>
2021-06-22 01:06:14 +0530

    conditionals program in java

---
+++ src/Conditionals/CheckAlphabet.java
@@ -0,0 +1,28 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 22-06-2021
+ *   Time: 00:58
+ *   File: CheckAlphabet.java
+ */
+
+package Conditionals;
+
+import java.util.Scanner;
+
+public class CheckAlphabet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the alphabet");
+        char ch = scanner.nextLine().charAt(0);
+        if(Character.isUpperCase(ch)){
+            System.out.println("alphabet is capital");
+        }
+        else
+            if(Character.isLowerCase(ch)) {
+            System.out.println("alphabet is small case");
+        }
+        else{
+            System.out.println("not an alphabet");
+        }
+    }
+}
