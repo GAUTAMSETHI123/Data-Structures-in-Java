5e2005791b45a31ab079cb4d0efa40ca95212e05
gautam <gautam.sethi_cs19@gla.ac.in>
2021-06-25 09:53:05 +0530

    created a node class in Deque

---
+++ src/Deque/Node.java
@@ -0,0 +1,21 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 25-06-2021
+ *   Time: 09:51
+ *   File: Node.java
+ */
+
+package Deque;
+
+public class Node {
+    int data;
+    Node next;
+    public Node(){
+        data = 0;
+        next = null;
+    }
+    public Node(int d){
+        data = d;
+        next = null;
+    }
+}
