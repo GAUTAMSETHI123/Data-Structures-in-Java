d086bf5b713de45d96aa148dade387c22af516a5
gautam <gautam.sethi_cs19@gla.ac.in>
2021-07-13 12:38:17 +0530

    swap alternate program in array

---
+++ src/Scratch/SwapAlterante.java
@@ -0,0 +1,44 @@
+package Scratch;
+
+import java.util.Scanner;
+
+public class SwapAlterante {
+    public static int[] takeInput(int size){
+        Scanner scn = new Scanner(System.in);
+
+        int []arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = scn.nextInt();
+
+        }
+        return arr;
+
+    }
+
+    public static int[] swap(int[] array){
+        for (int i = 0; i <= array.length-1; i=i+2) {
+            int temp = array[i];
+            array[i] = array[i+1];
+            array[i+1] = temp;
+
+        }
+        return array;
+    }
+    public static void print(int[] array1){
+        for (int val: array1
+        ) {
+            System.out.print(val+" ");
+
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int size1 = scn.nextInt();
+        int[] arr1 = takeInput(size1);
+        print(arr1);
+        System.out.println();
+        int[] arr2 = swap(arr1);
+        print(arr2);
+    }
+}
