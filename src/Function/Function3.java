ae76d76eb29d9b1cd5e9ca50246bd690322212ce
Gautam <gautamsethigagan@gmail.com>
2022-02-01 12:48:48 +0530

    change by value only in Java in Methods

---
+++ src/Function/Function3.java
@@ -0,0 +1,25 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 01-02-2022
+ *   Time: 12:38
+ *   File: Function3.java
+ */
+
+package Function;
+
+public class Function3 {
+    public static void main(String[] args) {
+        String name = "Gautam Sethi";
+        change(name);
+        System.out.println(name);
+    }
+
+    static void change(String name2){
+        name2 = " Jatin Singh";
+        System.out.println(name2);
+
+    }
+
+
+
+}
