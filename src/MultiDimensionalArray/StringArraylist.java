ff1da156a1b38701690ec09a61fc67b8bfcbb913
Gautam <gautamsethigagan@gmail.com>
2022-02-03 13:12:21 +0530

    String Arraylist in Java

---
+++ src/MultiDimensionalArray/StringArraylist.java
@@ -0,0 +1,23 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 03-02-2022
+ *   Time: 13:06
+ *   File: StringArraylist.java
+ */
+
+package MultiDimensionalArray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StringArraylist {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<String>> ls2d = new ArrayList<>();
+        ArrayList<String> x = new ArrayList<>();
+        x.add("Hello");
+        x.add("world!");
+        ls2d.add(x);
+
+        System.out.println(Arrays.deepToString(ls2d.toArray()));
+    }
+}
