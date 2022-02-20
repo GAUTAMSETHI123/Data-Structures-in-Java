0e762026417872315b76cfd2426dc2b9f2b5cbdd
Gautam <gautamsethigagan@gmail.com>
2022-02-20 14:27:24 +0530

    first recursion program

---
+++ src/CommunityClassroom/Recursion.java
@@ -0,0 +1,26 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 20-02-2022
+ *   Time: 14:24
+ *   File: Recursion.java
+ */
+
+package CommunityClassroom;
+
+import java.sql.SQLOutput;
+
+public class Recursion {
+    public static void main(String[] args) {
+
+        print(1);
+    }
+
+    static void print(int n){
+        if(n==5){
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }
+}
