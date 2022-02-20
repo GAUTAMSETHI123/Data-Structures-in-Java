14c9b5e011ae7804c781d912f04e2d80d524a579
Gautam <gautamsethigagan@gmail.com>
2022-02-20 15:34:36 +0530

    find nth fibonacci number using recursion

---
+++ src/FibonacciRecursion.java
@@ -0,0 +1,20 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 20-02-2022
+ *   Time: 15:26
+ *   File: FibonacciRecursion.java
+ */
+
+public class FibonacciRecursion {
+    public static void main(String[] args) {
+        int ans = fib(4);
+        System.out.println(ans);
+    }
+
+    static int fib(int n ){
+        if(n<2){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
