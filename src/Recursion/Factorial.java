e0e930d8706fd44c37742e933c9597ab8a369d0e
Gautam <gautamsethigagan@gmail.com>
2022-03-09 14:39:31 +0530

    Factorial of a number using recursion

---
+++ src/Recursion/Factorial.java
@@ -0,0 +1,24 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 09-03-2022
+ *   Time: 14:37
+ *   File: Factorial.java
+ */
+
+package Recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int ans = fact(5);
+        System.out.println(ans);
+
+    }
+
+    public static int fact(int n){
+        if(n<=1){
+            return 1;
+
+        }
+        return n* fact(n-1);
+    }
+}
