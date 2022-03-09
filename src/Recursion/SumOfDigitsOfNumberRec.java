ee8982a7df63449fcb0bf3d267b764c9c1a558c2
Gautam <gautamsethigagan@gmail.com>
2022-03-09 15:00:42 +0530

    sum of digits of a number using recursion

--- src/Recursion/SumOfDigitsOfNumberRec.java
+++ src/Recursion/SumOfDigitsOfNumberRec.java
@@ -9,13 +9,13 @@ package Recursion;
 
 public class SumOfDigitsOfNumberRec {
     public static void main(String[] args) {
-        int res = sumOfDigits(1342);
+        int res = sumOfDigits(1340);
         System.out.println(res);
     }
 
     public static int sumOfDigits(int n){
-        if(n==1){
-            return 1;
+        if(n ==0){
+            return 0;
 
         }
         return (n%10) + sumOfDigits(n/10);
