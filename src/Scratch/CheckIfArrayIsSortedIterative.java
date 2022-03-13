f1a0f46a95ab07eb478b88de4b0bf06b84a09015
Gautam <gautamsethigagan@gmail.com>
2022-03-13 23:04:42 +0530

    CheckIfArrayIsSorted using iterative approach.

--- src/Scratch/CheckIfArrayIsSortedIterative.java
+++ src/Scratch/CheckIfArrayIsSortedIterative.java
@@ -9,7 +9,7 @@ package Scratch;
 
 import java.util.Scanner;
 
-public class CheckIfArrayIsSorted {
+public class CheckIfArrayIsSortedIterative {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of the array");
