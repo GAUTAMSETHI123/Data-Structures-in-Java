a4af2066bb1ea52ea87ac97a013e9a6343ea07fd
Gautam <gautamsethigagan@gmail.com>
2022-03-20 23:42:02 +0530

    .

--- src/Scratch/CheckPrimes.java
+++ src/Scratch/CheckPrimes.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 import java.math.*;
 
-public class CheckPrime {
+public class CheckPrimes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number you want to check");
