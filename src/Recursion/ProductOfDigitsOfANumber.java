1128a0993c9a8e0429cf1b70c1a64d8fc717e43e
Gautam <gautamsethigagan@gmail.com>
2022-03-20 23:41:18 +0530

    .

--- src/Recursion/ProductOfDigitsOfANumber.java
+++ src/Recursion/ProductOfDigitsOfANumber.java
@@ -9,7 +9,7 @@ package Recursion;
 
 import java.util.Scanner;
 
-public class ProductOfDigitsOfNumber {
+public class ProductOfDigitsOfANumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
