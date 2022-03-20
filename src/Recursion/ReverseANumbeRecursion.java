653428d97e7acc8b641b6ab61570c5b5f30522d3
Gautam <gautamsethigagan@gmail.com>
2022-03-20 23:38:39 +0530

    .

--- src/Recursion/ReverseANumbeRecursion.java
+++ src/Recursion/ReverseANumbeRecursion.java
@@ -9,7 +9,7 @@ package Recursion;
 
 import java.util.Scanner;
 
-public class ReverseANumbeRec {
+public class ReverseANumbeRecursion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
