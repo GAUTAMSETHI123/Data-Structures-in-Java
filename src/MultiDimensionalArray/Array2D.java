46c9cfb33e96c5cdc95f583cbb9bca12f013c2f6
Gautam <gautamsethigagan@gmail.com>
2022-03-20 23:39:57 +0530

    .

--- src/MultiDimensionalArray/Array2D.java
+++ src/MultiDimensionalArray/Array2D.java
@@ -9,7 +9,7 @@ package MultiDimensionalArray;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Arrays2D {
+public class Array2D {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
