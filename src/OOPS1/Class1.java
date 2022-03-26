7f1f24af3bdf5133e24f71683100fd34bf5a964b
Gautam <gautamsethigagan@gmail.com>
2022-03-26 23:32:20 +0530

    class And Objects in java

---
+++ src/OOPS1/Class1.java
@@ -0,0 +1,57 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 26-03-2022
+ *   Time: 15:10
+ *   File: Class1.java
+ */
+
+package OOPS1;
+
+import java.util.Arrays;
+
+public class Class1 {
+    public static void main(String[] args) {
+//        Student [] student = new Student[5];
+//        System.out.println(Arrays.toString(student));
+
+        Student student1 = new Student(13, "Gautam", 56.9f);
+        student1.Greeting();
+
+
+        System.out.println(student1.Name);
+        System.out.println(student1.rollNo);
+        System.out.println(student1.marks);
+
+
+
+
+
+
+
+    }
+
+}
+
+class Student {
+    int rollNo;
+    String Name;
+    double marks;
+
+//    Student()
+//    {
+//        this.rollNo = 13;
+//        this.Name = "Gautam";
+//        this.marks = 89.8;
+//    }
+
+    Student(int roll, String Name, float marks){
+        this.marks = marks;
+        this.rollNo = roll;
+        this.Name = Name;
+    }
+
+
+    void Greeting(){
+        System.out.println("hello my name is "+ this.Name);
+    }
+}
