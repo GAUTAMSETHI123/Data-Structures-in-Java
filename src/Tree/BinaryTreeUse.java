b715f32b785c206164a59dc6468e248933495ae1
Gautam <gautamsethigagan@gmail.com>
2022-03-27 16:03:36 +0530

    printing a binary using pre-order Traversal

---
+++ src/Tree/BinaryTreeUse.java
@@ -0,0 +1,45 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 27-03-2022
+ *   Time: 15:24
+ *   File: BinaryTreeUse.java
+ */
+
+package Tree;
+
+public class BinaryTreeUse {
+
+    public static void printTree(BinaryTreeNode<Integer> root){
+
+        if(root == null){
+            return ;
+        }
+        System.out.println(root.data);
+        printTree(root.left);
+        printTree(root.right);
+
+    }
+
+
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
+        BinaryTreeNode<Integer> rootLeft = new BinaryTreeNode<>(2);
+        BinaryTreeNode<Integer> rootRight = new BinaryTreeNode<>(3);
+
+        root.left = rootLeft;
+        root.right= rootRight;
+
+
+        BinaryTreeNode<Integer> twoRight = new BinaryTreeNode<>(5);
+        BinaryTreeNode<Integer> threeLeft = new BinaryTreeNode<>(4);
+
+
+        rootLeft.right = twoRight;
+        rootRight.left = threeLeft;
+
+        printTree(root);
+
+
+
+    }
+}
