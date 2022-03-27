7944e16a74438a6582a9658da78b4f7738398c7c
Gautam <gautamsethigagan@gmail.com>
2022-03-27 15:23:59 +0530

    Node of a Binary Tree

---
+++ src/Tree/BinaryTreeNode.java
@@ -0,0 +1,15 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 27-03-2022
+ *   Time: 15:19
+ *   File: BinaryTreeNode.java
+ */
+
+package Tree;
+
+public class BinaryTreeNode<T> {
+    T data;
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
+
+}
