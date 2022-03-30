c59bec320a816a00b90d965cc2894fbe79ae6b90
Gautam <gautamsethigagan@gmail.com>
2022-03-30 23:31:52 +0530

    function added

--- src/Recursion/FirstIndexOfElementRecursive.java
+++ src/Recursion/FirstIndexOfElementRecursive.java
@@ -7,5 +7,32 @@
 
 package Recursion;
 
+import java.util.Scanner;
+
 public class FirstIndexOfElementRecursive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [] arr = {1,2,3,2,4};
+        int target = sc.nextInt();
+
+        int res = firstIndex(arr, target,0);
+        System.out.println(res);
+
+    }
+
+    public static int firstIndex(int [] arr1, int target1,int i){
+        if(i > arr1.length){
+            return -1;
+        }
+
+        if(arr1[i] == target1){
+            return i;
+        }
+        return firstIndex(arr1, target1,i+1);
+    }
+
+
+
+
 }
