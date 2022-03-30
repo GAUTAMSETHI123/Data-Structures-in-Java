80e76edfa3b6c2ee8ca4766edade4a060599e048
Gautam <gautamsethigagan@gmail.com>
2022-03-30 23:31:39 +0530

    function added

--- src/Recursion/LastIndexOfElementRecursive.java
+++ src/Recursion/LastIndexOfElementRecursive.java
@@ -7,5 +7,33 @@
 
 package Recursion;
 
+import com.sun.source.tree.ClassTree;
+
+import java.util.Scanner;
+
 public class LastIndexOfElementRecursive {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [] arr = {1,2,3,2,4};
+        int target = sc.nextInt();
+
+        int res = lastIndex(arr, target, arr.length-1);
+        System.out.println(res);
+
+    }
+
+    public static int lastIndex(int [] arr1, int target1,int i){
+
+        if(i<=0){
+            return -1;
+        }
+
+        if(arr1[i] == target1){
+            return i;
+        }
+        return lastIndex(arr1, target1,i-1);
+    }
+
 }
