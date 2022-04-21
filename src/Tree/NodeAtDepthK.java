ebd46b6533c4387046f0c0924213b7bca6bc869c
Gautam <gautamsethigagan@gmail.com>
2022-04-22 02:01:23 +0530

    Node At Depth K

---
+++ src/Tree/NodeAtDepthK.java
@@ -0,0 +1,23 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 22-04-2022
+ *   Time: 01:58
+ *   File: NodeAtDepthK.java
+ */
+
+package Tree;
+
+public class NodeAtDepthK {
+    public static void DepthK(BinaryTreeNode<Integer> root, int k){
+        if(root == null){
+            return;
+        }
+
+        if(k ==0){
+            System.out.print(root.data);
+            return;
+        }
+        DepthK(root.left, k-1);
+        DepthK(root.right, k-1);
+    }
+}
