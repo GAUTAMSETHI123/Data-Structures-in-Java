ae7fee4555e7c42e45cfc16ce2c23eeb6465fdcd
Gautam <gautamsethigagan@gmail.com>
2022-04-25 23:55:01 +0530

    Invert Binary Tree

---
+++ src/Tree/InvertBinaryTree.java
@@ -0,0 +1,32 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 25-04-2022
+ *   Time: 23:54
+ *   File: InvertBinaryTree.java
+ */
+
+package Tree;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) {
+            return root;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+
+        // Swapping the left and right nodes using temp variable
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // Repeat the same for left and right using recursion
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+
+    }
+}
