65cba60ad490363a5078efeac551ddb58314c179
Gautam <gautamsethigagan@gmail.com>
2022-04-26 23:56:29 +0530

    .

--- src/MultiDimensionalArray/Array2D.java
+++ src/MultiDimensionalArray/Array2D.java
@@ -13,11 +13,11 @@ public class Array2D {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [][] arr = new int[3][];
+        int [][] array = new int[3][];
 
-        for(int rows=0; rows< arr.length; rows++){
-            for(int col=0; col< arr[rows].length; col++){
-                arr[rows][col] = sc.nextInt();
+        for(int rows=0; rows< array.length; rows++){
+            for(int col=0; col< array[rows].length; col++){
+                array[rows][col] = sc.nextInt();
 
             }
         }
@@ -31,8 +31,8 @@ public class Array2D {
 //        }
 
 
-        for(int row=0; row<arr.length; row++){
-            System.out.println(Arrays.toString(arr[row]));  //output in the array matrix format.
+        for(int row=0; row<array.length; row++){
+            System.out.println(Arrays.toString(array[row]));  //output in the array matrix format.
 
             }
         }
